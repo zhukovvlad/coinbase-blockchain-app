706ce59b70711c04f5d4233031dc842621479a14
Zhukovvlad <zhukovvlad@yandex.ru>
2022-06-21 16:14:40 +0300

    Created initial page with wallet connect and disconnect options

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,63 @@ import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react'
 
 export default function Home() {
   const connectWithMetamask = useMetamask();
+  const disconnectFromMetamask = useDisconnect();
   const address = useAddress();
   return (
-    <div>
+    <Wrapper>
       {address ? (
-        <h4>Connected with {address}</h4>
+        <WalletConnect>
+          <h4>Connected with {address}</h4>
+          <Button onClick={disconnectFromMetamask}>Disconnect</Button>
+        </WalletConnect>
       ) : (
-        <button onClick={connectWithMetamask}>Connect Metamask Wallet</button>
+        <WalletConnect>
+          <Button onClick={connectWithMetamask}>Connect Metamask Wallet</Button>
+          <Details>
+            You need Chrome to be
+            <br /> able to run this app
+          </Details>
+        </WalletConnect>
       )}
-    </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  max-width: 100vw;
+  background-color: #0a0b0d;
+  color: white;
+  display: grid;
+  place-items: center
+`
+
+const WalletConnect = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const Button = styled.div`
+  border: 1px solid #282b2f;
+  padding: 0.8rem;
+  font-size: 1.3rem;
+  font-weight: 500;
+  border-radius: 0.4rem;
+  background-color: #3773f5;
+  color: #000;
+
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+const Details = styled.div`
+  font-size: 1.2rem;
+  text-align: center;
+  margin-top: 1rem;
+  font-weight: 500;
+  color: #282b2f;
+`
