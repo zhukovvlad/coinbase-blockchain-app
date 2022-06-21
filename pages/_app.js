d1b092a58e3f65c62bfd26f919058d6ec51fe122
Zhukovvlad <zhukovvlad@yandex.ru>
2022-06-21 14:55:41 +0300

    Installed an initiated ThirdWeb

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import { ThirdwebProvider, ChainId } from '@thirdweb-dev/react';
+
+const desiredChainId = ChainId.Rinkeby;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider desiredChainId={desiredChainId}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  )
 }
 
 export default MyApp
